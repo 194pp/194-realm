6b236cbf54029518341158a3bfe3afc659eb8985
194pp <zilvinas111@gmail.com>
2020-12-20 15:32:04 +0200

    Persons.js component setup

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -3,10 +3,11 @@ import Person from "./Person/Person";
 
 const persons = (props) => props.persons.map((person, index) => {
     return <Person
-      click={() => this.deletePersonHandler(index)}
+      click={() => props.clicked(index)}
       name={person.name}
       key={person.id}
       age={person.age}
-      changed={(event) => this.nameChangedHandler(event, person.id)}/>
-  }
-)
+      changed={(event) => props.changed(event, person.id)}/>
+  });
+
+export default persons;
